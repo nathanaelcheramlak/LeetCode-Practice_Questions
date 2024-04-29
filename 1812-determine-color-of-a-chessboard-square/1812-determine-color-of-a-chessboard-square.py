eb16ed594d5146f0b51d9021f84ad79edb0184dc
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-04-30 02:53:26 +0300

    Time: 9 ms (85.78%), Space: 11.8 MB (23.53%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type coordinates: str
         :rtype: bool
         """
-        my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
-        if ( my_dict[coordinates[0]] + int(coordinates[-1]) ) % 2 == 0:
+        if ( ord(coordinates[0]) + int(coordinates[-1]) ) % 2 == 0:
             return False
         return True
