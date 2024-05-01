df86dd0c3ee43af69b1a3ba460d821f6eeedb12a
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-05-01 22:34:33 +0300

    Time: 8 ms (84.70%), Space: 11.6 MB (42.37%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -4,7 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0:
-            return 0
-        m = n ** 0.5
-        return int(m)
+        return int(sqrt(n))
