8c403577e5a18f84a016385f0c4030fa2e5cfc94
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-05-07 21:08:55 +0300

    Time: 16 ms (54.88%), Space: 11.6 MB (75.14%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,5 +1,10 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
         index = 0
         for i in range(len(nums)):
             if nums[i] != val:
