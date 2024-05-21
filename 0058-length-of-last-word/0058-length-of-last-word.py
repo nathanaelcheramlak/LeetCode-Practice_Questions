c69d4f30cbd3a6b51a9e3a1dc3f41b34f17f242f
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-05-21 20:27:31 +0300

    Time: 17 ms (25.47%), Space: 11.9 MB (24.95%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,19 +1,8 @@
 class Solution(object):
-    def lengthOfLastWord(self, word):
+    def lengthOfLastWord(self, s):
         """
         :type s: str
         :rtype: int
         """
-        isSpace = True
-        l = len(word) - 1
-        lastW = 0
-
-        while l >= 0:
-            if word[l] != ' ':
-                lastW += 1
-                isSpace = False  
-            elif not isSpace and lastW > 0:
-                break
-            l -= 1
-
-        return lastW
+        s = s.rstrip()
+        return len(s.split()[-1])
