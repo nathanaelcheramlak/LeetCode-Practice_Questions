9065f862a405eff8ac64e52fb3ccd8b415140109
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-08-11 20:31:31 +0300

    Time: 539 ms (63.34%), Space: 13.5 MB (14.69%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -5,13 +5,12 @@ class Solution(object):
         :rtype: List[int]
         """
         i = len(arr) - 1
-        maxElement = arr[i]
+        maxElement = -1
         result = [0 for _ in range(len(arr))]
 
         while i >= 0:
-            if i == len(arr) - 1:
-                result[i] = -1
-            elif arr[i] >= maxElement:
+
+            if arr[i] >= maxElement:
                 result[i] = maxElement
                 maxElement = arr[i]
             else:
