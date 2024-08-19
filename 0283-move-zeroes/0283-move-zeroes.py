120363a5b0925be0b7779a9ecf2b9e1b4549ad96
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-08-19 14:41:57 +0300

    Time: 125 ms (29.32%), Space: 12.7 MB (86.55%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # Two pointer approach
+        left = 0
+        right = 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
