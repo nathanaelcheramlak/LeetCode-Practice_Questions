ec3fd98462be421ec63899bf9cf00f03cd04a2a0
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-08-19 14:42:42 +0300

    Time: 101 ms (99.07%), Space: 12.6 MB (99.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -12,3 +12,6 @@ class Solution(object):
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
             right += 1
+            
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
