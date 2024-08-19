33b69dc6bfb6ea040443a9987232ee21f19eab00
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-08-19 20:17:33 +0300

    Time: 656 ms (23.42%), Space: 16 MB (40.78%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        my_bills = {5: 0, 10: 0, 20: 0}  # We start with no cash
+        for bill in bills:
+            my_bills[bill] += 1
+            
+            if bill == 10:
+                if my_bills[5] < 1:
+                    return False
+                my_bills[5] -= 1  # $5 change
+            
+            elif bill == 20:
+                if my_bills[10] > 0 and my_bills[5] > 0:
+                    # $10 and one $5
+                    my_bills[10] -= 1
+                    my_bills[5] -= 1
+                elif my_bills[5] >= 3:
+                    # Three $5 bills
+                    my_bills[5] -= 3
+                else:
+                    return False  # Not enough change available
+        
+        return True
