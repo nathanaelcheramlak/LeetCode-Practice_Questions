7e4ebc31e15a1d1994b0128874cfe1042c2d9053
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-08-29 19:56:23 +0300

    Time: 14 ms (88.70%), Space: 11.6 MB (38.91%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,11 +7,15 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        # Bruteforce
-        i = 0
-        while m < len(nums1):
-            nums1[m] = nums2[i]
-            i += 1
-            m += 1
-            
-        return nums1.sort()
+        last = m + n - 1
+        m -= 1
+        n -= 1
+
+        while n >= 0:
+            if m >= 0 and nums1[m] > nums2[n]:
+                nums1[last] = nums1[m]
+                m -= 1
+            else:
+                nums1[last] = nums2[n]
+                n -= 1
+            last -= 1
