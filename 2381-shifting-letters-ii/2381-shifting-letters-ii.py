43a46dd8095882dfbb29cb814857d72c7a830932
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2024-11-28 23:38:12 +0300

    Time: 55 ms (83.44%), Space: 41.3 MB (37.85%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,17 +1,23 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        arr = [0] * len(s)
-        for shift in shifts:
-            for i in range(shift[0], shift[1] + 1):
-                if shift[-1] == 0: # Shift Backward
-                    arr[i] -= 1
-                else: # Shift Forward
-                    arr[i] += 1
-        
-        for i in range(len(arr)):
-            ascii_code = ord(s[i]) + arr[i]
-            val = chr((ascii_code - 97) % 26 + 97) # To make it circular.
-            arr[i] = val
-        
-        return ''.join(arr)
+        n = len(s)
+        arr = [0] * (n + 1) # The last element is useless its there to prevent out of bound errors
+
+        for left, right, direction in shifts:
+            if direction == 0: # Shift backward
+                arr[left] -=  1
+                arr[right + 1] += 1
+            else: # Shift forward
+                arr[left] += 1
+                arr[right + 1] -= 1
+
+        # Prefix summer
+        for i in range(1, n):
+            arr[i] += arr[i - 1]
+
+        result = []
+        for i in range(n):
+            result.append(chr((ord(s[i]) + arr[i] - 97) % 26 + 97))
+
+        return ''.join(result)
             
