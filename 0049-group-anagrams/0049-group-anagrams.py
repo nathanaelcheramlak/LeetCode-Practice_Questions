b3770676d4ac9aa85fb8bbe065bb395307220fb7
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-12-20 22:04:36 +0300

    Time: 15 ms (49.76%), Space: 21.9 MB (36.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,12 +2,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash_map = defaultdict(list)
         for word in strs:
-            char_count = Counter(word)
-            count_list = []
-            for alp in string.ascii_lowercase:
-                freq = char_count[alp]
-                count_list.append(freq)
-            count_tup = tuple(count_list)
+            char_count = [0] * 26
+            for letter in word:
+                idx = ord(letter) - ord("a")
+                char_count[idx] += 1
+            count_tup = tuple(char_count)
             hash_map[count_tup].append(word)
         
         return list(hash_map.values())
