96dcecc800a29cd6adec1ecfaee8fcf8b12e8439
Nathanael Cheramlak <124700160+nathanaelcheramlak@users.noreply.github.com>
2025-12-20 22:02:40 +0300

    Time: 75 ms (5%), Space: 21.9 MB (36.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,13 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        dic = dict()
-        grouped_ls = []
-        for st in strs:
-            sorted_st = "".join(sorted(st))
-            if sorted_st not in dic:
-                dic[sorted_st] = [st]
-            else:
-                dic[sorted_st].append(st)
-        for value in dic.values():
-            grouped_ls.append(value)
-        return grouped_ls
-                
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        for word in strs:
+            char_count = Counter(word)
+            count_list = []
+            for alp in string.ascii_lowercase:
+                freq = char_count[alp]
+                count_list.append(freq)
+            count_tup = tuple(count_list)
+            hash_map[count_tup].append(word)
+        
+        return list(hash_map.values())
